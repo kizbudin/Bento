e5786df339e40d316a765bd3d25160c396288dd4
M kizbudin <mkizbudin@gmail.com>
2020-11-02 22:58:43 +0700

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,15 +3,15 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change your name
-var name = 'FallCrescent';
+var name = 'FallCrescent' ;
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
+var gree1 = 'Go to Sleep!';
+var gree2 = 'Good morning!';
+var gree3 = 'Good afternoon';
+var gree4 = 'Good evening';
+var gree5 = 'Good evening';
+var gree6 = 'Good evening';
 
 // Define the hours of the greetings
 if (Hr >= 23 && Hr < 5) {
